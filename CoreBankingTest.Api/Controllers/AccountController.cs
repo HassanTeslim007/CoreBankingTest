2643d7890d4609d83b9aaeb6b2f13469303531dd
HassanTeslim007 <hassanteslim12@gmail.com>
2025-10-27 14:21:23 +0100

    intitial commit

---
+++ CoreBankingTest.Api/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using CoreBankingTest.Core.Interfaces;
+using CoreBankingTest.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreBankingTest.Api.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountsController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllAccounts()
+        {
+            var accounts = _accountRepository.GetAll();
+            return Ok(accounts);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAccountById(int id)
+        {
+            var account = _accountRepository.GetById(id);
+            if (account == null)
+            {
+                return NotFound($"Account with ID {id} not found");
+            }
+            return Ok(account);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAccount(AccountModel account)
+        {
+            _accountRepository.Add(account);
+            return CreatedAtAction(nameof(GetAccountById), new { id = account.Id }, account);
+        }
+    }
+}
---
+++ CoreBankingTest.Api/Program.cs
@@ -0,0 +1,42 @@
+
+using CoreBankingTest.Core.Interfaces;
+using CoreBankingTest.DAL.Repositories;
+
+namespace CoreBankingTest.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Register dependencies (DI)
+            builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
